eaec27298953f37ba4e16b6fbe1acaa2718c711c
LytTheBit <francesco.bonaiuti@gmail.com>
2025-09-11 11:08:35 +0200

    Aggiornato l'URL del server per la generazione delle immagini

--- generator_app/views.py
+++ generator_app/views.py
@@ -88,7 +88,7 @@ def train_lora(request):
         })
 
 # Codice per la generazione di immagini tramite un server IA esterno
-LAMBDA_SERVER_URL = "http://127.0.0.1:8000/generate"
+LAMBDA_SERVER_URL = "http://127.0.0.1:8010/generate"
 
 def generate_image(request):
     if request.method != "POST":
